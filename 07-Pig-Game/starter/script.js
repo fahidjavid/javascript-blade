2535801dd39c1d0e842d1e384b8903e2368db41d
Fahid Javid <fahidjavid@gmail.com>
2023-04-06 01:44:46 +0500

    Added initial targeting of the elements in the pig game.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const firstScore = document.getElementById('score--0');
+const secondScore = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+// Hide the dice element
+dice.classList.add('hidden');
+
+// Reset the score
+firstScore.textContent = 0;
+secondScore.textContent = 0;
